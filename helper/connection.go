eb9d4c96fe7ad26676076ed43b6dba0c5353ea16
anurag sharma <anurag@msd-my.com>
2020-08-13 17:01:38 +0800

    add mongo connection and collection

---
+++ helper/connection.go
@@ -0,0 +1,55 @@
+package helper
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// ConnectDB : This is helper function to connect mongoDB
+
+func ConnectDB() *mongo.Collection {
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("go_db").Collection("users")
+
+	return collection
+}
+
+// ErrorResponse : This is error model.
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+// GetError : This is helper function to prepare error model.
+
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
